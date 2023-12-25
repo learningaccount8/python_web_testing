440b6219a58c9259e7bb4af89b333d2daeec4b2b
learningaccount8 <fortesting0123450@gmail.com>
2023-12-25 12:31:06 +0200

    refactoring

--- test_ui_layout/test_login_to_app.py
+++ test_ui_layout/test_login_to_app.py
@@ -11,6 +11,6 @@ def test_user_go_to_w3school(set_up):
     page.locator("xpath=//h3[contains(text(),'W3Schools Online Web Tutorials')]").click()
     expect(page.locator("xpath=//input[@id='search2']")).to_be_visible()
     page.get_by_role("link", name="JAVA", exact=True).click()
-    page.locator("//a[contains(text(),'Java Booleans')]").click()
+    page.locator("xpath=//a[contains(text(),'Java Booleans')]").click()
     expect(page.get_by_role("heading", name="Boolean Values")).to_be_visible()
 
